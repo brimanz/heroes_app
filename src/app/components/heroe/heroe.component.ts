4f9cd31db4849edaa60232dccf70885d10a79696
jonathan briceño <brimanz2014@gmail.com>
2023-06-18 17:41:40 -0400

    adding getHeroe method on services file

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from '../../services/heroes.service';
 
 
 @Component({
@@ -8,13 +9,15 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class HeroeComponent {
   
+  heroe:any = {};
 
-
-  constructor( private activatedRoute: ActivatedRoute ){ 
+  constructor( private activatedRoute: ActivatedRoute,
+               private _heroesService: HeroesService
+   ){ 
 
     this.activatedRoute.params.subscribe( params => {
-      console.log( params['id'] );
-    })
+      this.heroe = this._heroesService.getHeroe( params['id'] );
+    });
   }
 
 
