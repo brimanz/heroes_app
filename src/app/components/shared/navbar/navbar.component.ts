b42970189c773e70df7e55b5abe0b5db36831f8e
jonathan briceno <brimanz2014@gmail.com>
2023-07-13 21:25:24 -0400

    adding buscador component

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-navbar',
@@ -6,14 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent {
 
-  constructor() {
+  constructor( private router:Router) {
   }
 
   ngOnInit(){
   }
 
   heroSearch(termino:string){
-    console.log(termino);
+    //console.log(termino);
+    this.router.navigate( ['/buscar', termino]);
   }
 
 }
